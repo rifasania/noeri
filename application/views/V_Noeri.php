a6bc23c1b992a59b086553df89f121061e0ceb93
Rif_San <rifasania06@gmail.com>
2023-06-04 15:08:04 +0700

    commit_rifa

--- application/views/V_Noeri.php
+++ application/views/V_Noeri.php
@@ -86,7 +86,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center justify-content-center">
     <div class="container position-relative">
- <!-- komen -->
+      <h1>Welcome to Noeri Kitchen</h1>
+      <h1>Welcome to Noeri Kitchen</h1>
+      <h2>"Simphoni Rasa, Pesta untuk Jiwa"</h2>
+      <h2>"Simphoni Rasa, Pesta untuk Jiwa"</h2>
       <a href="#about" class="btn-get-started scrollto">Get Started</a>
     </div>
   </section><!-- End Hero -->
