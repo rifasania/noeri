9bc206a6c6367aabfb14826f76c5400d3c82a913
Rif_San <rifasania06@gmail.com>
2023-06-04 14:52:33 +0700

    commit_rifa

--- application/views/V_Noeri.php
+++ application/views/V_Noeri.php
@@ -88,6 +88,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="container position-relative">
       <h1>Welcome to Noeri Kitchen</h1>
       <h2>"Simphoni Rasa, Pesta untuk Jiwa"</h2>
+      <h2>"Simphoni Rasa, Pesta untuk Jiwa"</h2>
       <a href="#about" class="btn-get-started scrollto">Get Started</a>
     </div>
   </section><!-- End Hero -->
