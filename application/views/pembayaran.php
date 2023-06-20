98abf15523a3f988f1f9dc35619caf329ac15d08
Ainun <nurainunlubis24@gmail.com>
2023-06-20 09:32:10 +0700

    footer

--- application/views/pembayaran.php
+++ application/views/pembayaran.php
@@ -46,7 +46,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
       <h1 class="logo">
       <a href="" class="logo"><img src="<?php echo base_url();?>/assets/img/1.png" alt="" class="img-fluid"></a>
-      <a href="">noeriKitchen</a>
+      <a href="">Noeri Kitchen</a>
       </h1>
 
       <nav id="navbar" class="navbar">
@@ -166,81 +166,72 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       </div>
     </section><!-- End Contact Section -->
 
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
-
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>Baker</h3>
+ <!-- ======= Footer ======= -->
+ <footer id="footer">
+			<div class="footer-top">
+				<div class="container">
+					<div class="row">
+					<div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Noeri Kitchen</h3>
             <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
+              Jl. Setia budi No 241 <br>
+              Bandung, Indonesia<br>
+              <br>
               <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
+              <strong>Email:</strong> <p>noeri_kitchen@gmail.com<br>noeri.kitchen@gmail.com</p>
             </p>
           </div>
+						<div class="col-lg-2 col-md-6 footer-links">
+							<h4>Useful Links</h4>
+							<ul>
+								<li>
+									<i class="bx bx-chevron-right"></i> <a href="#">Home</a>
+								</li>
+								<li>
+									<i class="bx bx-chevron-right"></i> <a href="#">About us</a>
+								</li>
+								<li>
+									<i class="bx bx-chevron-right"></i>
+									<a href="#">Terms of service</a>
+								</li>
+								<li>
+									<i class="bx bx-chevron-right"></i>
+									<a href="#">Privacy policy</a>
+								</li>
+							</ul>
+						</div>
 
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
+						<div class="col-lg-3 col-md-6 footer-links">
             <h4>Our Services</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Delivery Order</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Ala carte</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Prasmanan</a></li>
             </ul>
           </div>
 
-          <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Join Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
+					</div>
+				</div>
+			</div>
 
-        </div>
-      </div>
-    </div>
-
-    <div class="container d-md-flex py-4">
-
-      <div class="me-md-auto text-center text-md-start">
-        <div class="copyright">
-          &copy; Copyright <strong><span>Baker</span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/baker-free-onepage-bootstrap-theme/ -->
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
-      </div>
-      <div class="social-links text-center text-md-right pt-3 pt-md-0">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
+			<div class="container d-md-flex py-4">
+				<div class="me-md-auto text-center text-md-start">
+					<div class="copyright">
+						&copy; Copyright <strong><span>NOERI KITCHEN</span></strong
+						>. All Rights Reserved
+					</div>
+		
+				</div>
+				<div class="social-links text-center text-md-right pt-3 pt-md-0">
+					<a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+					<a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+					<a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+					<a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+					<a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+				</div>
+			</div>
+		</footer>
+		<!-- End Footer -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
